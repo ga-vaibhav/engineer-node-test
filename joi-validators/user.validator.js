3c8046a20873d6523e0e12d0071d007bf2aa6af4
Vaibhav Patel <vaibhav.patel@green-apex.com>
2020-05-20 17:14:48 +0530

    fixed minor validation issue

--- joi-validators/user.validator.js
+++ joi-validators/user.validator.js
@@ -1,8 +1,8 @@
 const Joi = require('@hapi/joi');
 const createUser = Joi.object().keys({
-    firstName: Joi.number().required(),
-    lastName: Joi.number().required(),
-    email: Joi.number().required()
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    email: Joi.string().email().required()
 });
 
 module.exports = {
